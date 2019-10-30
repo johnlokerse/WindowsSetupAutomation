0204bbb47ae5578ef6b57417fa30d711007ff1c6
John <J.G.Lokerse@gmail.com>
2019-10-30 22:26:44 +0100

    Automation wip

---
+++ JohnWindowsInstall.ps1
@@ -0,0 +1,43 @@
+# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Work software
+choco install dotnetcore-sdk -y
+choco install git.install -y
+choco install linqpad -y
+choco install vscode -y
+choco install visualstudio2019enterprise -y
+choco install visualstudio2019-workload-netweb -y
+choco install visualstudio2019-workload-azure -y
+choco install visualstudio2019-workload-node -y
+choco install visualstudio2019-workload-manageddesktop -y
+choco install visualstudio2019-workload-netcoretools -y
+choco install microsoft-teams.install -y
+choco install sql-server-management-studio -y
+choco install office365proplus -y
+choco install docker-cli -y
+choco install nodejs.install
+
+# Personal software
+choco install logitech-options -y
+choco install everything -y
+choco install whatsapp -y
+choco install geforce-experience -y
+choco install steam -y
+choco install dropbox -y
+
+# Shared software
+choco install googlechrome -y
+choco install adobereader -y
+choco install putty.install -y
+
+# VSCode extensions
+code --install-extension eamodio.gitlens
+code --install-extension ms-vscode.csharp
+code --install-extension zignd.html-css-class-completion
+code --install-extension eg2.vscode-npm-script
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension johnpapa.angular-essentials
+code --install-extension zhuangtongfa.material-theme
